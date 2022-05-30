ab43aa6dec263e29abd92ac8fe2a380d4520a2b9
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-05-30 15:24:02 -0700

    commit tree 037749db8b9c84f28935c919c4929616955157fd

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(c,d) {
+    return c - d;
+}
+
+function multiply(a,b) {
+    return a * b;
+
+
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function subtract(b,a) {
+    return b - a;
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+
+
+
+function makeInt(n) {
+    const interger = parseInt(n,10); {
+     return interger;
+    }
+}
+
+
+
+function preserveDecimal(n) {
+    const decimal = parseFloat(n); { 
+    return decimal;
+    }
+}
+
+
+
+
